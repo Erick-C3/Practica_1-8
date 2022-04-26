51258b78fd54de4ec2605f86f1f5b1baece2e873
Erick <erick.nahuelpaiva.001@gmail.com>
2022-04-26 12:21:01 -0300

    modificada carpeta ejemplos

--- ejemplos/algoritmo.js
+++
@@ -1,8 +0,0 @@
-var velocidadInicial = 0;
-  var aceleracion = 20;
-  var velocidadFinal = 60;
-  var tiempoParaAlcanzarVelFinal = "no definido" ;
-
-  tiempoParaAlcanzarVelFinal = velocidadInicial + aceleracion * tiempoParaAlcanzarVelFinal ;
-
-  console.log (tiempoParaAlcanzarVelFinal)
